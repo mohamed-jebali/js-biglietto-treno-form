0723b5635b2257f80ad8e8a7648bbcd9e618b816
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-15 16:52:36 +0200

    added to fixed and euro console log

--- js/script.js
+++ js/script.js
@@ -1,28 +1,25 @@
-// let inputDistance = parseInt(document.getElementById("InputDistance"));
-// let inputAge = parseInt(document.getElementById("inputAge"));
-// let risultato = inputDistance * 0.233;
-// let prezzoScontato;
+
 let btn = document.querySelector("button.btn");
 
 
 
 btn.addEventListener('click', function() {
 
-    let inputDistance = parseInt(document.getElementById("InputDistance").value = "distance");
-    let inputAge = parseInt(document.getElementById("inputAge").value = "number");
+    let inputDistance = parseInt(document.getElementById("InputDistance").value);
+    let inputAge = parseInt(document.getElementById("inputAge").value);
     let risultato = inputDistance * 0.233;
     let prezzoScontato;
 
 if(inputAge > 120 || inputAge < 0){
-    console.log("Inserisci un età valida")
+    console.log("Inserisci un' età valida");
 }
 else if(inputAge < 18) {
     prezzoScontato = risultato - (0.194 * risultato);
-    console.log(prezzoScontato);
+    console.log(prezzoScontato.toFixed(2) + " €");
 }
 else if(inputDistance > 65) {
     prezzoScontato = risultato - (0.377 * risultato);
-    console.log(prezzoScontato);
+    console.log(prezzoScontato.toFixed(2) + " €");
 }
 else{
     console.log(prezzoScontato);
