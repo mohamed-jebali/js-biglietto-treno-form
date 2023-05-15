9abb7fa7a17fe8d5712f3b2f0333d9dc6873546e
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-15 19:40:32 +0200

    added bootstrap classes to improve layout form

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 
 let btn = document.querySelector("button.btn");
 let message;
-let messageRecap;
+let recapAge;
+let recapDistance;
 
 
 btn.addEventListener('click', function() {
+    
 
     let inputDistance = parseInt(document.getElementById("InputDistance").value);
     let inputAge = parseInt(document.getElementById("inputAge").value);
@@ -15,30 +17,36 @@ if(inputAge > 120 || inputAge <= 0){
     console.log("Inserisci un' età valida");
     message = "Inserisci un' età valida";
     prezzoScontato = 0;
-    messageRecap = "Inserisci dati";
+    recapDistance = "Inserisci dati";
+    recapAge = "Inserisci dati";
 }
 else if(inputAge < 18) {
     prezzoScontato = risultato - (0.194 * risultato);
     console.log(prezzoScontato + " €");
     message = "è stato applicato lo sconto del 19.4% per i passeggeri minorenni";
-    messageRecap = (" età del passeggero " + inputAge) + (" /distanza percorsa " + inputDistance + "Km")
+    recapDistance = ("distanza percorsa " + inputDistance + "Km");
+    recapAge = ("eta del passeggero " + inputAge);
 }
 else if(inputAge > 65) {
     prezzoScontato = risultato - (0.377 * risultato);
     console.log(prezzoScontato + " €");
     message = "è stato applicato lo sconto del 37.7% per i passeggeri over 65";
-    messageRecap = (" età del passeggero " + inputAge) + (" /distanza percorsa " + inputDistance + "Km")
+    recapDistance = ("distanza percorsa " + inputDistance + "Km");
+    recapAge = ("eta del passeggero " + inputAge);
 }
 else{
     console.log(prezzoScontato);
     message = "non è stato applicato lo sconto";
-    messageRecap = (" età del passeggero " + inputAge) + (" /distanza percorsa " + inputDistance + "Km")
+    recapDistance = ("distanza percorsa " + inputDistance + "Km");
+    recapAge = ("eta del passeggero " + inputAge);
     prezzoScontato = risultato;
 }
 
-document.getElementById("output").innerHTML += prezzoScontato.toFixed(2) + "€ " + message;
 
-document.getElementById("recap").innerHTML += messageRecap;
+
+document.getElementById("output").innerHTML += prezzoScontato.toFixed(2) + "€ " + message;
+document.getElementById("recapAge").innerHTML += recapAge;
+document.getElementById("recapDistance").innerHTML += recapDistance;
 
 });
 
