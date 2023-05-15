53487fa8870632f20563d3418ce81faf1c8821b3
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-15 15:18:22 +0200

    added html and js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let inputDistance = document.getElementById("InputDistance")
+let inputAge = document.getElementById("inputAge");
+let risultato = inputDistance * 0.233;
+
+if(inputAge < 18) {
+    prezzoScontato = risultato - (0.194 * risultato);
+}
+else if(inputDistance > 65) {
+    prezzoScontato = risultato - (0.377 * risultato);
+}
+else{
+    document.getElementById("output").innerHTML = risultato.toFixed(2) + "€";
+}
