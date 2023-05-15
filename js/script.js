90cb234c73b3d1d06c2aeb171c86bfbeed2f0ad5
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-15 16:19:15 +0200

    working on function

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
-let inputDistance = parseInt(document.getElementById("InputDistance"));
-let inputAge = parseInt(document.getElementById("inputAge"));
-let risultato = inputDistance * 0.233;
-let prezzoScontato;
+// let inputDistance = parseInt(document.getElementById("InputDistance"));
+// let inputAge = parseInt(document.getElementById("inputAge"));
+// let risultato = inputDistance * 0.233;
+// let prezzoScontato;
 let btn = document.querySelector("button.btn");
 
 
 
 btn.addEventListener('click', function() {
 
-    let distance = parseInt(document.getElementById("InputDistance").value = "distance");
-    let age = parseInt(document.getElementById("inputAge").value = "number");
+    let inputDistance = parseInt(document.getElementById("InputDistance").value = "distance");
+    let inputAge = parseInt(document.getElementById("inputAge").value = "number");
+    let risultato = inputDistance * 0.233;
+    let prezzoScontato;
 
-if(age > 120 || inputAge < 0){
+if(inputAge > 120 || inputAge < 0){
     console.log("Inserisci un età valida")
 }
-else if(age < 18) {
+else if(inputAge < 18) {
     prezzoScontato = risultato - (0.194 * risultato);
     console.log(prezzoScontato);
 }
-else if(distance > 65) {
+else if(inputDistance > 65) {
     prezzoScontato = risultato - (0.377 * risultato);
     console.log(prezzoScontato);
 }
+else{
+    console.log(prezzoScontato);
+}
 });
-// else{
-//     document.getElementById("output").innerHTML = risultato.toFixed(2) + "€";
-// }
