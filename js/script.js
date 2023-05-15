51f56c8ef29e9c71b21152825c92c0919a23eab6
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-15 16:12:40 +0200

    working on button eventListener

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-let inputDistance = document.getElementById("InputDistance")
-let inputAge = document.getElementById("inputAge");
+let inputDistance = parseInt(document.getElementById("InputDistance"));
+let inputAge = parseInt(document.getElementById("inputAge"));
 let risultato = inputDistance * 0.233;
-let btn = document.querySelector(".btn");
-console.log(btn);
+let prezzoScontato;
+let btn = document.querySelector("button.btn");
 
-if(inputAge < 18) {
+
+
+btn.addEventListener('click', function() {
+
+    let distance = parseInt(document.getElementById("InputDistance").value = "distance");
+    let age = parseInt(document.getElementById("inputAge").value = "number");
+
+if(age > 120 || inputAge < 0){
+    console.log("Inserisci un età valida")
+}
+else if(age < 18) {
     prezzoScontato = risultato - (0.194 * risultato);
+    console.log(prezzoScontato);
 }
-else if(inputDistance > 65) {
+else if(distance > 65) {
     prezzoScontato = risultato - (0.377 * risultato);
+    console.log(prezzoScontato);
 }
+});
 // else{
 //     document.getElementById("output").innerHTML = risultato.toFixed(2) + "€";
 // }
